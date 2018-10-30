dd7aa03429fbc096203e465238c5f815e32e9987
Pavle Jonoski <pavle.jonoski@keitaro.com>
2018-10-30 14:59:05 +0100

    Fixed linter errors.

--- microkubes/security/keys.py
+++ microkubes/security/keys.py
@@ -9,7 +9,7 @@ from os import listdir, sep as path_sep
 from os.path import isdir, isfile, abspath, basename
 
 
-class KeyError(Exception):
+class KeyException(Exception):
     """Represents an error that occurs during loading or specifying a key.
     """
     pass
@@ -21,26 +21,26 @@ class Key:
     :param key_file: ``str``, the path to the key file.
     :param public: ``bool``, flag indicating whether this key is public.
 
-    :raises: :class:`KeyError` if the key file does not exist or is not a file.
+    :raises: :class:`KeyException` if the key file does not exist or is not a file.
     """
     def __init__(self, key_file, public=False):
         self.public = public
         self.key_file = key_file
         if not isfile(self.key_file):
-            raise KeyError('File %s does not exist or is not a file.' % self.key_file)
-    
+            raise KeyException('File %s does not exist or is not a file.' % self.key_file)
+
     def load(self):
         """Tries to load a key from the given file.
 
         Returns the content of the key as bytes.
 
-        :raises: :class:`KeyError` if an error occurs during loading.
+        :raises: :class:`KeyException` if an error occurs during loading.
         """
         try:
             with open(self.key_file, 'rb') as kf:
                 return kf.read()
         except Exception as read_err:
-            raise KeyError from read_err
+            raise KeyException from read_err
 
 
 class KeyStore:
@@ -66,23 +66,22 @@ class KeyStore:
             self._load_keys_from_dir(dir_path)
         if keys is not None:
             self._load_keys_from_map(keys)
-    
 
     def get_key(self, key_name=None):
         """Retrieves a :class:`Key` by name.
 
         :param key_name: ``str``, the name of the key to retrieve. If not given, the default value is ``default``.
 
-        :returns: :class:`Key` if the key is found; otherwise raises :class:`KeyError`.
+        :returns: :class:`Key` if the key is found; otherwise raises :class:`KeyException`.
 
         """
         if key_name is None:
             key_name = 'default'
         key = self.keys.get(key_name)
         if not key:
-            raise KeyError('No such key: %s' % key_name)
+            raise KeyException('No such key: %s' % key_name)
         return key
-    
+
     def get_private_key(self, key_name):
         """Retrieves a key flagged as private by its name.
 
@@ -90,19 +89,19 @@ class KeyStore:
 
         :param key_name: ``str``, the name of the key to retrieve. If not given, the default value is ``default``.
 
-        :returns: :class:`Key` if the key is found; otherwise raises :class:`KeyError`.
+        :returns: :class:`Key` if the key is found; otherwise raises :class:`KeyException`.
 
         """
         key = self.get_key(key_name=key_name)
         if key.public:
-            raise KeyError('No such private key: %s' % key_name)
+            raise KeyException('No such private key: %s' % key_name)
         return key
-    
+
     def public_keys(self):
         """Retrieves all of the public keys as a dict key_name => :class:`Key`.
         """
-        return {key_name: key for key in self.keys if key.public}
-    
+        return {key_name: key for key_name, key in self.keys.items() if key.public}
+
     def add_key(self, key_name, key_file):
         """Add a key to this :class:`KeyStore`.
 
@@ -111,11 +110,11 @@ class KeyStore:
 
         """
         self._load_keys_from_map({key_name: key_file})
-    
+
     def _load_keys_from_dir(self, dir_path):
         if not isdir(dir_path):
-            raise KeyError('Path %s is not a directory.' % dir_path)
-        
+            raise KeyException('Path %s is not a directory.' % dir_path)
+
         for file_name in listdir(dir_path):
             abs_file_name = abspath(dir_path + path_sep + file_name)
             if isfile(abs_file_name):
@@ -125,15 +124,14 @@ class KeyStore:
                     self.keys[key_name[:-4]] = Key(abs_file_name, public=True)
                 elif lwr_name.endswith('.pem') or '.' not in lwr_name:
                     self.keys[key_name.split('.')[0]] = Key(abs_file_name, public=False)
-    
+
     def _load_keys_from_map(self, keys):
         for key_name, key_file in keys.items():
             if not isfile(key_file):
-                raise KeyError('Key file %s does not exist or it is not a file.' % key_file)
+                raise KeyException('Key file %s does not exist or it is not a file.' % key_file)
             abs_key_file = abspath(key_file)
             base_name = basename(abs_key_file)
             priv_key = True
             if base_name.lower().endswith('.pub'):
-                priv_key=False
-            
+                priv_key = False
             self.keys[key_name] = Key(abs_key_file, public=not priv_key)
